5cef2b6d9df79cc36cb8675a48f436f43f6dc08d
sweta1312 <swetaverma131296@gmail.com>
2021-12-23 12:55:57 +0530

    git demo

---
+++ practice/Corejava/src/com/practice/FirstExample.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class FirstExample {
+    int a=10;
+    public static void main(String[] args) {
+        FirstExample obj=new FirstExample();
+        System.out.println(obj.a);
+
+    }
+}
